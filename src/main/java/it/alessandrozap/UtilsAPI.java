01197476dbc380cca4796cd289a6715ced5c5c3f
Alessandro <alessandroc160707@gmail.com>
2025-05-05 22:40:52 +0200

    Some fixes, optimitations & Code Cleanup

--- src/main/java/it/alessandrozap/UtilsAPI.java
+++ src/main/java/it/alessandrozap/UtilsAPI.java
@@ -1,48 +1,30 @@
 package it.alessandrozap;
 
 import lombok.Getter;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.reflections.Reflections;
 
 public class UtilsAPI {
 
     @Getter
     private static UtilsAPI instance;
-
     @Getter
     private final JavaPlugin plugin;
+    @Getter
+    private final String prefix;
+    @Getter
     private boolean inizialized = false;
 
-    private Listener listener;
-
-    public UtilsAPI(JavaPlugin plugin) throws Exception {
+    public UtilsAPI(JavaPlugin plugin, String prefix) throws Exception {
         if(plugin == null) throw new Exception();
         this.plugin = plugin;
+        this.prefix = prefix;
 
         UtilsAPI.instance = this;
+        init();
     }
 
     public void init() {
-        //this.plugin.getServer().getPluginManager().registerEvents();
         this.inizialized = true;
     }
-
-    /*
-    public void unload() {
-        UtilsAPI.getScheduler().cancelTasks();
-        HandlerList.unregisterAll(this.listener);
-    }
-    */
-
-
-    /*
-    public static TaskScheduler getScheduler() {
-        return UniversalScheduler.getScheduler(InventoryAPI.getInstance().getPlugin());
-    }
-
-    public static InventoryAPI getInstance() {
-        return InventoryAPI.instance;
-    }*/
 
 }
