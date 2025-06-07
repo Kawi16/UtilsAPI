5fc72c7106a9c75e08be8d62ce23f8608d5b8f33
Alessandro <alessandroc160707@gmail.com>
2025-06-07 17:18:45 +0200

    Fixed Logger

--- src/main/java/it/alessandrozap/logger/Logger.java
+++ src/main/java/it/alessandrozap/logger/Logger.java
@@ -5,9 +5,13 @@ import it.alessandrozap.managers.messages.Locale;
 import org.bukkit.Bukkit;
 
 public class Logger {
-    private static final String CONSOLE_PREFIX = Locale.translate("", UtilsAPI.getInstance().getPrefix());
+    private static String CONSOLE_PREFIX = UtilsAPI.getInstance() != null ?
+            Locale.translate(UtilsAPI.getInstance().getPrefix()) :
+            Locale.PREFIX.getMessage(false);
 
     public static void log(String message, LogType type) {
         Bukkit.getConsoleSender().sendMessage(CONSOLE_PREFIX + type.getColor() + message);
     }
+
+    public static void setConsolePrefix(String prefix) { CONSOLE_PREFIX = prefix; }
 }
